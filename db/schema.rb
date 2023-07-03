47b3b67b93602e35f1447b1eb55b4ac01b9b1034
shasherazi <hassanrandomz@gmail.com>
2023-07-03 13:54:34 +0500

    Fix belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_082533) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_084226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_082533) do
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_entities_on_author_id"
+  end
+
+  create_table "entities_groups", id: false, force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["entity_id"], name: "index_entities_groups_on_entity_id"
+    t.index ["group_id"], name: "index_entities_groups_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
