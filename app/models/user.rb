96a65238b19a27f714789802076d4cd1429a8e96
shasherazi <hassanrandomz@gmail.com>
2023-07-03 15:42:08 +0500

    Add user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :groups
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
